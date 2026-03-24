d4a59e6c61685ec96dd1d7968d094906a98ba922
dynasti121 <donramond3@gmailL.com>
2026-03-23 19:19:31 -0700

    update

--- chamber/scripts/javascript.js
+++ chamber/scripts/javascript.js
@@ -9,7 +9,7 @@ button.addEventListener("click", () => {
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.querySelector(".span3").textContent = new Date().toLocaleDateString('en-US', options);
 
-const userName = "Edem Francis";
+const userName = "obinali uchenna";
 document.querySelector(".span2").textContent = userName;
 
 
